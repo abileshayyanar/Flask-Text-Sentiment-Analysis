abf1e76b86f356e3462362b163b7a361ea7c5667
Abilesh Ayyanar <abileshayyanar01@gmail.com>
2025-09-17 12:40:35 -0600

    First commit, created flask app

---
+++ app.py
@@ -0,0 +1,85 @@
+from flask import Flask, request, render_template, redirect, session
+import mysql.connector
+from sentiments import second
+import os
+
+app = Flask(__name__)
+
+# Initialize user cookies
+app.secret_key = os.urandom(24)
+
+# Blueprint to call second python file
+app.register_blueprint(second)
+
+# Establish database connection
+try:
+    conn = mysql.connector.connect(
+        host="localhost", 
+        user="root", 
+        password="Enter you mysql password here", 
+        database="user_db")
+    cursor = conn.cursor()
+except:
+    print("An exception occurred")
+
+# Call login template
+@app.route('/')
+def login():
+    return render_template('login.html')
+
+# Call register template
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/home')
+def home():
+    if 'user_id' in session:
+        return render_template('home.html')
+    else:
+        return redirect('/')
+    
+@app.route('/login_validation', methods=['POST'])
+def login_validation():
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    cursor.execute(
+        """SELECT * 
+        FROM 'users' 
+        WHERE 'email' LIKE '{}' AND 'password' LIKE '{}'""".format(email, password))
+    users = cursor.fetchall()
+
+    if len(users) > 0:
+        session['user_id'] = users[0][0]
+        return redirect('/home')
+    else:
+        return redirect('/login')
+    
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    name = request.form.get('uname')
+    password = request.form.get('upassword')
+    email = request.form.get('uemail')
+
+    cursor.execute(
+        """INSERT INTO 'users' ('name', 'email', 'password')
+        VALUES ('{}', '{}', '{}')""".format(name, email, password))
+    conn.commit()
+    cursor.execute(
+        """SELECT *
+        FROM 'users'
+        WHERE 'email' LIKE '{}'""".format(email))
+    myuser = cursor.fetchall()
+    session['user_id'] = myuser[0][0]
+    return redirect('/home')
+
+@app.route('/logout')
+def logout():
+    session.pop('user_id')
+    return redirect('/')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
