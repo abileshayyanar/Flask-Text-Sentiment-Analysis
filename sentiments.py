64f14adf7713671e88451ca627e82410046ab60a
Abilesh Ayyanar <abileshayyanar01@gmail.com>
2025-09-18 14:02:33 -0600

    almost completed downloadData function

--- sentiments.py
+++ sentiments.py
@@ -18,13 +18,27 @@ def sentiment_analyzer():
 # Main logic
 class SentimentAnalysis:
 
-    # 
+    # Constructor
     def __init__(self):
         self.tweets = []
         self.tweetText = []
 
-
+    # Function to connect to tweepy api and download tweet data
     def DownloadData(self, keyword, tweets):
+
+        neutral = 0
+        polarity = 0
+
+        #variables for positive sentiment
+        pos = 0
+        strong_pos = 0
+        weak_pos = 0
+        
+        # variables for negative sentiment
+        neg = 0
+        strong_neg = 0
+        weak_neg = 0
+
         consumerKey = 'get your consumer key from twitter'
         consumerSecret = 'get your consumerSecret from twitter'
         accessToken = 'get your access token from twitter'
@@ -33,4 +47,17 @@ class SentimentAnalysis:
         auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
         auth.set_access_token(accessToken, accessTokenSecret)
         api = tweepy.API(auth, wait_on_rate_limit=True)
+
+        tweets = int(tweets)
+
+        # Search for tweets
+        self.tweets = tweepy.Cursor(api.search_tweets, q=keyword, lang="en").items(tweets)
+
+        # Open a file to append to
+        csvFile = open('results.csv', 'a')
+        # Use csv writer
+        csvWriter = csv.writer(csvFile)
+
+
+
 
